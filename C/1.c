3409658d6d4612decd2a935ba9ca362b1ae61a78
dimik3 <mgtsby@yandex.ru>
2017-02-11 12:31:15 +0300

    Add files via upload

---
+++ C/1.c
@@ -0,0 +1,50 @@
+﻿#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Задание 1: В одномерном массиве, заполненном случайными числами, определить
+минимальный и максимальный элементы.*/
+
+#define SIZE 10
+void randomNumber(int massiv[],int size);
+void minMaxNumber(int massiv[],int size,int minN,int maxN);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int massiv[SIZE];
+	int minN = 0;
+	int maxN = 0;
+	randomNumber(massiv, SIZE);
+	minMaxNumber(massiv, SIZE, minN, maxN);
+	return 0;
+}
+
+void randomNumber(int massiv[],int size)
+{
+	srand(time(NULL));
+	for (int i = 0; i < size; i++)
+	{
+		massiv[i] = rand() % 10;
+		printf("Случайное число: %d\n", massiv[i]);
+	}
+}
+
+void minMaxNumber(int massiv[],int size,int minN,int maxN)
+{
+	for (int j = 0; j<size; j++)
+	{
+		if (massiv[j] < massiv[minN])  // Если следущий элемент меньше минимального, то...
+		{
+			minN = j;
+		} // назначаем его минимальным.
+		if (massiv[j] > massiv[maxN])  // Если следущий элемент больше максимального, то...   
+		{
+			maxN = j;
+		} // назначаем его максимальным.
+	}
+	printf("\nМинимальное число = %d.", massiv[minN]); // Выводим минимальный элемент.
+	printf("\nМаксимальное число = %d.", massiv[maxN]); // Выводим максимальный элемент.
+	getchar();
+}
