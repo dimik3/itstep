b289893cd3e34bd0d3b0c1604ef2b3ba501f8307
dimik3 <mgtsby@yandex.ru>
2017-04-26 12:02:30 +0300

    26.04.2017

--- C/Classroom/FILE*.c
+++ C/Classroom/FILE*.c
@@ -1,59 +1,103 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <locale.h>
-
-struct StockExchangeCourses
+ 
+struct Header
 {
-	float BuyDollar;
-	float SellDollar;
-	float BuyEuro;
-	float SellEuro;
+    int RecordCount;
+    int CurrencyCount;
+    int Data;
+    int CurrencyCount2;
+    int Data2;
 };
-
-void writeСurrencyRate(struct StockExchangeCourses*  stockExchangeCourses, FILE* stockExchange)
+ 
+struct CurrencyRates
 {
-	fwrite(stockExchangeCourses, sizeof(struct StockExchangeCourses), 1, stockExchange);
-	/*printInfo(stockExchange);*/
-}
-
-void entreСurrencyRate()
-{
-	FILE* stockExchange = fopen("D:\\Sheblykin\\Блок ввода и вывода данных\\db.bin", "wb");
-	struct StockExchangeCourses*  stockExchangeCourses = malloc(sizeof(struct StockExchangeCourses));
-	printf("Please, entre Dollar rate (Buy): ");
-	scanf("%f", &stockExchangeCourses->BuyDollar); 
-	printf("Please, entre Dollar rate (Sell): ");
-	scanf("%f", &stockExchangeCourses->SellDollar);
-	printf("Please, entre Dollar rate (Buy): ");
-	scanf("%f", &stockExchangeCourses->BuyEuro);
-	printf("Please, entre Euro rate (Sell): ");
-	scanf("%f", &stockExchangeCourses->SellEuro);
-	writeСurrencyRate(stockExchangeCourses, stockExchange);
-
-
-}
-
-
-void printInfo(FILE* stockExchange)
-{
-	struct StockExchangeCourses* buff = malloc(sizeof(struct StockExchangeCourses));
-	fread(buff, sizeof(struct StockExchangeCourses), 1, stockExchange);
-	printf("BuyDollar: %.2f\n", buff->BuyDollar);
-	printf("SellDollar: %.2f\n", buff->SellDollar);
-	printf("BuyEuro: %.2f\n", buff->BuyEuro);
-	printf("SellEuro: %.2f\n", buff->SellEuro);
-}
-
+    float BynToUsd;
+    float UsdToByn;
+    float BynToEur;
+    float EurToByn;
+    char BankName[256];
+};
+ 
+#define FILE_NAME "db.bin"
+ 
 int main()
 {
-	FILE* stockExchange = fopen("D:\\Sheblykin\\Блок ввода и вывода данных\\db.bin", "rb");
-	if (stockExchange == NULL)
-	{
-		entreСurrencyRate();
-	}
-	else
-	{
-		printInfo(stockExchange);
-	}
+    char headerData[200];
+ 
+ 
+    struct Header header;
+    header.RecordCount = 2;
+    header.CurrencyCount2 = 5;
+    header.Data2 = 67;
+ 
+    if (sizeof(struct Header) > sizeof(headerData))
+    {
+        printf("Error!");
+        return -1;
+    }
+ 
+    memcpy(headerData, &header, 200);
+ 
+    struct CurrencyRates currency;
+    FILE* rates = fopen(FILE_NAME, "rb");
+ 
+    if (rates == NULL)
+    {
+        rates = fopen(FILE_NAME, "wb");
+       
+        printf("BYN to USD: ");
+        scanf("%f", &currency.BynToUsd);
+        printf("\nUSD to BYN: ");
+        scanf("%f", &currency.UsdToByn);
+        printf("\nBYN to EUR: ");
+        scanf("%f", &currency.BynToEur);
+        printf("\nEUR to BYN: ");
+        scanf("%f", &currency.EurToByn);
+        strcpy(currency.BankName, "hello");
+ 
+        fwrite(&headerData, sizeof(headerData),
+            1, rates);
+        fwrite(&currency, sizeof(struct CurrencyRates),
+            1, rates);
+        fwrite(&currency, sizeof(struct CurrencyRates),
+            1, rates);
+ 
+        fclose(rates);
+        return 0;
+    }
+ 
+    fread(&header, sizeof(struct Header),
+        1, rates);
+    fseek(rates, 200, SEEK_SET);
+    fread(&currency, sizeof(struct CurrencyRates),
+        1, rates);
+    printf("Records: %i\n\n", header.RecordCount);
+    printf("\nBYN to USD: %.4f\n", currency.BynToUsd);
+    printf("USD to BYN: %.4f\n", currency.UsdToByn);
+    printf("BYN to EUR: %.4f\n", currency.BynToEur);
+    printf("EUR to BYN: %.4f\n", currency.EurToByn);
+ 
+    fread(&currency, sizeof(struct CurrencyRates),
+        1, rates);
+    printf("\nBYN to USD: %.4f\n", currency.BynToUsd);
+    printf("USD to BYN: %.4f\n", currency.UsdToByn);
+    printf("BYN to EUR: %.4f\n", currency.BynToEur);
+    printf("EUR to BYN: %.4f\n", currency.EurToByn);
+ 
+    fseek(rates, 0, SEEK_END);
+    int const fileSize = ftell(rates);
+ 
+    if ((header.RecordCount * sizeof(struct CurrencyRates) +
+        sizeof(headerData)) == fileSize)
+    {
+        printf("Bank: %s\n", currency.BankName);
+    }
+    else
+    {
+        printf("Bank: -\n");
+    }
+ 
+    fclose(rates);
+    return 0;
 }
