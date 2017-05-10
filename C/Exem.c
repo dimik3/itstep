96e5648a294029a4d1fa5fa8b83b71a662c2e979
dimik3 <mgtsby@yandex.ru>
2017-05-10 13:09:57 +0300

    Update Exem.c

--- C/Exem.c
+++ C/Exem.c
@@ -1,91 +1,253 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 #include <locale.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <direct.h>
 
-#define SizeName 256
-#define SizeText 1024
 
-enum Recipe
+/*
+Разработать программу хранения рецептов.
+При запуске программа показывает меню.
+Меню:
+1. Добавление рецепта
+2. Просмотр рецепта
+3. Редактирование рецепта
+4. Удаление рецепта
+5. Выход
+Каждый рецепт должен содержать следующую информацию:
+1. Дату создания рецепта
+2. Имя рецепта
+3. Текст рецепта
+4. Рейтинг (от 1 до 10)
+Пункт №1 спрашивает имя рецепта на английском языке (или транслите). Это имя в
+последствии будет именем файла на файловой системе в котором будет лежать весь тест.
+Расширение файла - *.recipe. Далее программа спрашивает текст рецепта и рейтинг.
+Пункт №2 выводит список рецептов на файловой системе (файлы с маской *.recipe рядом
+с приложением) у которых размер файла больше 0. Далее выводит на экран содержимое
+рецепта (дата, имя, текст, рейтинг).
+Пункт №3 выводит список рецептов на файловой системе (файлы с маской *.recipe рядом
+с приложением) у которых размер файла больше 0. Затем программа спрашивает новый
+текст рецепта, который заменит весь текущий текст.
+Пункт №4 выводит список рецептов на файловой системе (файлы с маской *.recipe рядом
+с приложением), затем спрашивает какой из тестов надо удалить и очищает файл (размер
+0).
+Пункт №5 – выход из программы.
+Очистка файла: можно открыть файл в режиме "w" и закрыть.
+*/
+
+
+struct Recipe
 {
-	RecipeAdd = 1,
-	RecipeView,
-	RecipeEditing,
-	RecipeDeleting,
-	Exit
-}recipe;
-
-struct RecipeInfo
-{
-	int DateOfRecipe;
-	char RecipeName[SizeName];
-	char RecipeText[SizeText];
-	int Rating;
+	char* name[50];
+	char* date[30];
+	char* text[1024];
+	char* rating[10];
 };
 
-void printMenu()
-{
-	printf("\tМеню:\n");
-	printf("1.Добавление рецепта\n");
-	printf("2.Просмотр рецепта\n");
-	printf("3.Редактирование рецепта\n");
-	printf("4.Удаление рецепта\n");
-	printf("5.Выход\n");
-	scanf("%i", &recipe);
-}
 
-void printRecipeInfo(struct RecipeInfo* recipeInfo)
+enum Menu
 {
-	printf("1.Дата создания рецепта:\t%i\n", recipeInfo->DateOfRecipe);
-	printf("2.Имя рецепта:\t%s\n", recipeInfo->RecipeName);
-	printf("3.Текст рецепта:\t%s\n", recipeInfo->RecipeText);
-	printf("4.Рейтинг(от1 до10):\t%i\n", recipeInfo->Rating);
-	
-}
+	AddRecipe = '1',
+	VieRecipe = '2',
+	EditRecipe = '3',
+	DelReciep = '4',
+	Exit = '0',
+};
 
-void AddRecipe(struct RecipeInfo* recipeInfo,FILE* RecipeFile)
-{
-		printf("Введите имя рецепта: ");
-		scanf("%s", &recipeInfo->RecipeName);
-		strcat(recipeInfo->RecipeName, ".recipe");
-		RecipeFile = fopen(recipeInfo->RecipeName, "w");
-		printf("Введите текст рецепта: ");
-		scanf("%s", &recipeInfo->RecipeText);
-		fputs(recipeInfo->RecipeText, RecipeFile);
-		fclose(RecipeFile);
-		printf("Рейтинг рецепта %s (от1 до10): ", recipeInfo->RecipeName);
-		scanf("%i", &recipeInfo->Rating);
-}
+void Recipe(struct Recipe recipe[]);
+void FileNull(FILE* fp);
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	struct RecipeInfo* recipeInfo = malloc(sizeof(struct RecipeInfo));
-	FILE* RecipeFile;
-	printMenu();
+	struct Recipe* recipe = malloc(1024 * sizeof(struct Recipe));	
+	printf("\t\t =*=*=*=*=*=*= Книга рецептов =*=*=*=*=*=*= \n\n");
+	Recipe(recipe);
+	return 0;
+}
+
+void FileNull(FILE* fp)
+{
+	if (fp == NULL)
+	{
+		printf("Не удалось открыть файл!\n");
+		getch();
+		exit(0);
+	}
+}
+
+void Recipe(struct Recipe recipe[])
+{
+	char* fileName = (char *)malloc(200);
+	char* disk = (char *)malloc(200);
+	char* folderName = (char *)malloc(200);
+	char* Bonding = (char *)malloc(200);
+	char* dir = (char *)malloc(200);
 
 	while (true)
 	{
-		switch (recipe)
+		printf("Выберите пункт меню:\n");
+		printf("1 - Добавить рецепт\n2 - Просмотр рецепта\n3 - Редактирование рецепта\n4 - Удаление рецепта\n0 - Выход\n");
+		FILE* fp;
+		char pressKey = getch();
+		switch (pressKey)
 		{
-		case RecipeAdd:
-			AddRecipe(recipeInfo, &RecipeFile);
+		case AddRecipe:
+			printf("Введите имя диска:\n");
+			fgets(disk, 50, stdin);
+			printf("Введите имя папки:\n");
+			fgets(folderName, 50, stdin);
+			printf("Введите имя рецепта:\n");
+			fgets(fileName, 50, stdin);
+
+			char* nameRecipe[256];
+			strcpy(nameRecipe, fileName);
+
+			disk[strlen(disk) - 1] = '\0';
+			folderName[strlen(folderName) - 1] = '\0';
+			fileName[strlen(fileName) - 1] = '\0';
+
+			strcpy(Bonding, disk);
+			strcat(Bonding, ":\\");
+			strcat(Bonding, folderName);
+			strcat(fileName, ".recipe");
+			mkdir(Bonding);
+			strcpy(dir, Bonding);
+			strcat(dir, "\\");
+			strcat(dir, fileName);
+
+			fp = fopen(dir, "w");
+			FileNull(fp);
+
+			strcpy(recipe->name, &nameRecipe);
+			printf("Введите дату рецепта(дд.мм.гггг):\n");
+			fgets(recipe->date, 50, stdin);
+			printf("Введите рейтинг рецепта(1..10):\n");
+			fgets(recipe->rating, 50, stdin);
+			printf("Введите текст рецепта:\n");
+			fgets(recipe->text,1024,stdin);
+
+			fwrite(recipe,sizeof(struct Recipe),1,fp);
+			fclose(fp);
 			break;
-		case RecipeView:
-			printRecipeInfo(recipeInfo);
+
+		case VieRecipe:
+			printf("Введите имя диска:\n");
+			fgets(disk, 50, stdin);
+			printf("Введите имя папки:\n");
+			fgets(folderName, 50, stdin);
+			printf("Введите имя рецепта:\n");
+			fgets(fileName, 50, stdin);
+
+			disk[strlen(disk) - 1] = '\0';
+			folderName[strlen(folderName) - 1] = '\0';
+			fileName[strlen(fileName) - 1] = '\0';
+
+			strcpy(Bonding, disk);
+			strcat(Bonding, ":\\");
+			strcat(Bonding, folderName);
+			strcat(fileName, ".recipe");
+			mkdir(Bonding);
+			strcpy(dir, Bonding);
+			strcat(dir, "\\");
+			strcat(dir, fileName);
+
+			fp = fopen(dir, "r");
+			FileNull(fp);
+
+			fread(recipe, sizeof(struct Recipe),1,fp);
+			printf("Имя рецепта: %s", recipe->name);
+			printf("===============================================\n");
+			printf("Дата рецепта: %s\n", recipe->date);
+			printf("===============================================\n");
+			printf("Текст: \n%s\n", recipe->text);
+			printf("===============================================\n");
+			printf("Рейтинг: %s", recipe->rating);
+			printf("===============================================\n");
+			fclose(fp);
 			break;
-		case RecipeEditing:
+
+		case EditRecipe:
+			printf("Введите имя диска:\n");
+			fgets(disk, 50, stdin);
+			printf("Введите имя папки:\n");
+			fgets(folderName, 50, stdin);
+			printf("Введите имя рецепта:\n");
+			fgets(fileName, 250, stdin);
+
+			disk[strlen(disk) - 1] = '\0';
+			folderName[strlen(folderName) - 1] = '\0';
+			fileName[strlen(fileName) - 1] = '\0';
+
+			strcpy(Bonding, disk);
+			strcat(Bonding, ":\\");
+			strcat(Bonding, folderName);
+			strcat(fileName, ".recipe");
+			mkdir(Bonding);
+			strcpy(dir, Bonding);
+			strcat(dir, "\\");
+			strcat(dir, fileName);
+
+			fp = fopen(dir, "w");
+			FileNull(fp);
+			
+			printf("Введите дату рецепта(дд.мм.гггг):\n");
+			fgets(recipe->date, 50, stdin);
+			printf("Введите рейтинг рецепта(1..10):\n");
+			fgets(recipe->rating, 50, stdin);
+			printf("Введите текст рецепта:\n");
+			fgets(recipe->text, 1024, stdin);
+			
+			fwrite(recipe, sizeof(struct Recipe), 1, fp);
+			fclose(fp);
 			break;
-		case RecipeDeleting:
+
+		case DelReciep:
+			printf("Введите имя диска:\n");
+			fgets(disk, 50, stdin);
+
+			printf("Введите имя папки:\n");
+			fgets(folderName, 256, stdin);
+
+			printf("Введите имя рецепта:\n");
+			fgets(fileName, 256, stdin);
+
+			disk[strlen(disk) - 1] = '\0';
+			folderName[strlen(folderName) - 1] = '\0';
+			fileName[strlen(fileName) - 1] = '\0';
+
+			strcpy(Bonding, disk);
+			strcat(Bonding, ":\\");
+			strcat(Bonding, folderName);
+			strcat(fileName, ".recipe");
+			mkdir(Bonding);
+			strcpy(dir, Bonding);
+			strcat(dir, "\\");
+			strcat(dir, fileName);
+
+			FileNull(fp);
+			if (remove(dir) == -1)
+			{
+				printf("Ошибка!\n");
+			}
+			else
+			{
+				printf("Удалено..\n");
+			}
 			break;
 		case Exit:
-			exit(0);
+			return 0;
+			break;
 		default:
-			printf("Не верный выбор,попробуй снова.\n");
+			printf("\nВы ввели неверное значение...\n\n");
 			break;
 		}
+
 		printf("\n");
-		printMenu();
 	}
+	free(fileName);
+	free(disk);
+	free(folderName);
+	free(Bonding);
+	free(dir);
 }
